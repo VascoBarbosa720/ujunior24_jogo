8bd2ac45f5dc7a39d6e02c47518be156372748c4
VascoBarbosa720 <49347927+VascoBarbosa720@users.noreply.github.com>
2024-07-05 17:10:16 +0100

    Update jogo.py

--- jogo.py
+++ jogo.py
@@ -6,7 +6,7 @@ import math
 # pygame setup
 pygame.init()
 pygame.display.set_caption('Flappy Bird do Vasco e do André')
-screen = pygame.display.set_mode((1280, 720), pygame.FULLSCREEN)
+screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
 
 running = True
